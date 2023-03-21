beb6174d2089882c8c4a06831c46ff9098e5d145
Christopher Penrose <christopher.penrose@reifyhealth.com>
2023-03-21 09:51:37 -0700

    formatting adjustments

--- src/main/java/com/getindata/kafka/connect/iceberg/sink/IcebergChangeEvent.java
+++ src/main/java/com/getindata/kafka/connect/iceberg/sink/IcebergChangeEvent.java
@@ -152,7 +152,7 @@ public class IcebergChangeEvent {
                                      JsonNode node) {
     String fieldTypeName = field.doc();
     LOGGER.debug("Processing Field:{} Type:{} Doc:{}",
-                field.name(), field.type(), fieldTypeName);
+                 field.name(), field.type(), fieldTypeName);
 
     final Object val;
     switch (field.type().typeId()) {
@@ -173,17 +173,21 @@ public class IcebergChangeEvent {
         break;
       case STRING:
         // string destination coercions based upon schema 'name' annotations
-        if (IcebergChangeEvent.coerceDebeziumDate && fieldTypeName.equals("io.debezium.time.Date")) {
-          val = node.isNull() ? null : LocalDate.ofEpochDay(node.asInt()).toString();
+        if (IcebergChangeEvent.coerceDebeziumDate &&
+            fieldTypeName.equals("io.debezium.time.Date")) {
+          val = node.isNull() ? null
+                : LocalDate.ofEpochDay(node.asInt()).toString();
         }
         else {
           if (IcebergChangeEvent.coerceDebeziumMicroTimestamp &&
               fieldTypeName.equals("io.debezium.time.MicroTimestamp")) {
-            val = node.isNull() ? null : Instant.ofEpochSecond(0L, node.asLong() * 1000).toString();
+            val = node.isNull() ? null
+                  : Instant.ofEpochSecond(0L, node.asLong() * 1000).toString();
           }
           else {
             // if the node is not a value node (method isValueNode returns false), convert it to string.
-            val = node.isValueNode() ? node.asText(null) : node.toString();
+            val = node.isValueNode() ? node.asText(null)
+                  : node.toString();
           }
         }
         break;
